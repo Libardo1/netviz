bfc3b7f5c8a518e2b8fa65796e0ca66a212b3986
Andrew Taber <andrew.e.taber@gmail.com>
2013-10-25 17:02:32 -0700

    stop using matplotlib animate, hack together solution

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a project I created for Rackspace hackday 10/25/2013
+
+The idea is to use the NetworkX graph library for Python as well as Matplotlib to visual particles as they move in a directed random graph.
--- directed_stochastic_flow.py
+++ directed_stochastic_flow.py
@@ -1,14 +1,11 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import animation
 
 class Particle:
   current_node = None
   def __init__(self, node):
     self.current_node = node
-
-fig = plt.figure(figsize=(8,8))
 
 G=nx.scale_free_graph(200)
 for e in G.edges():
@@ -24,26 +21,8 @@ for e in G.edges():
     
 pos = nx.random_layout(G)
 
-def init():
-
-  nx.draw_networkx_edges(G,pos,edge_color=weights,
-                          alpha=0.4,edge_cmap=plt.cm.jet,
-                          arrows=False,
-                          edge_vmin=min(weights),edge_vmax=max(weights))
-
-  occupancy = [len(G.node[n]['particles']) for n in G.nodes()]
-  node_weights = { n:particles for n,particles in zip(G.nodes(),occupancy) }
-
-# NOTE: matplotlib cmaps throws an error here if node_color length is not the same as G length. GH issue?
-  nx.draw_networkx_nodes(G,pos,
-                         node_color=node_weights.values(),
-                         node_size=20,
-                         alpha=.6,
-                         cmap=plt.cm.jet,
-                         vmin=min(node_weights.values()),
-                         vmax=max(node_weights.values()))
-
 def animate(i):
+  fig = plt.figure(figsize=(8,8))
   for particle in particle_list:
     n = particle.current_node
     successors = { successor:G.edge[n][successor]["weight"] for successor in G.neighbors(n) }
@@ -63,7 +42,7 @@ def animate(i):
       particle.current_node = next_node
 
   nx.draw_networkx_edges(G,pos,edge_color=weights,
-                          alpha=0.4,edge_cmap=plt.cm.jet,
+                          alpha=0.2,edge_cmap=plt.cm.jet,
                           arrows=False,
                           edge_vmin=min(weights),edge_vmax=max(weights))
 
@@ -72,14 +51,16 @@ def animate(i):
 
   nx.draw_networkx_nodes(G,pos,
                          node_color=node_weights.values(),
-                         node_size=20,
+                         node_size=40,
                          alpha=.6,
                          cmap=plt.cm.jet,
                          vmin=min(node_weights.values()),
                          vmax=max(node_weights.values()))
+  plt.axis("off")
+  plt.savefig('figure#%s' %i)
+  plt.close()
   return
 
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200)
-
-plt.axis("off")
-plt.show()
+if __name__ == "__main__":
+  for i in range(20):
+    animate(i)
